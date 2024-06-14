5cd4c9f9376fde9cd7141d3116421f498481a408
Haseeb Ali <smhaseebali8@gmail.com>
2024-06-14 05:52:31 +0500

    Created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Routes, Route} from 'react-router'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+      </Routes>
+    </>
+
   )
 }
 
