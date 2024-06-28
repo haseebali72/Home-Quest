fe2ecec42d6fb51460f40f441fc0179ef3ef6ab4
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-06-29 03:21:27 +0500

    added another Loader in Loader.jsx

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ColorRing } from 'react-loader-spinner'
 
-const ColorCircleLoader = () => {
+const ColorCircleLoaderFull = () => {
   return (
     <>
       <div className='bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0'>
@@ -23,4 +23,21 @@ const ColorCircleLoader = () => {
   )
 }
 
-export { ColorCircleLoader }
+const ColorCircleLoader = () => {
+  return (
+    <>
+        <div className='flex justify-center items-center w-full h-full'>
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
+        </div>
+    </>
+  )
+}
+export { ColorCircleLoaderFull, ColorCircleLoader }
