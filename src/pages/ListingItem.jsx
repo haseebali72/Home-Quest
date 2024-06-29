c7b796345e560e88eaf8686939fde1718f3a29c5
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-06-29 07:12:46 +0500

    Created structure and added CSS to ListingItems

--- src/pages/ListingItem.jsx
+++ src/pages/ListingItem.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
+// import Moment from 'react-moment'
+import { NavLink } from 'react-router-dom'
+import { MdLocationOn } from "react-icons/md";
 
-const ListingItem = ({listing, id}) => {
+const ListingItem = ({ listing, id }) => {
+  const timestamp = new Date(listing.timestamp.seconds * 1000);
   return (
     <>
-        <div className=''>
-          {listing.name}
-        </div>
+      <li className='bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px] '>
+        <NavLink to={`/category/${listing.category}/${id}`} className="contents">
+          <img src={listing.imageURLs[0]} alt='ghar' className='h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in ' loading='lazy'/>
+          {/* <Moment className= "absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"format='undefined'>{timestamp}</Moment>         */}
+          <div className=" w-full p-[10px] ">
+            <div className=" flex items-center space-x-1 ">
+                <MdLocationOn className='h-4 w-4 text-green-600 '/>
+                <p className='font-semibold text-sm mb-[2px] text-gray-600 truncate'>{listing.address}</p>
+            </div>
+            <p className='font-semibold m-0 text-xl  '>{listing.name}</p>
+            <p className='text-[#457b9d] mt-2 font-semibold '>
+              {`${listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} ${listing.currency}`}
+              {listing.category == "rent"? "/month" : null}
+             </p>
+             <div className='flex  items-center mt-[10px] space-x-3 '>
+                <div className='flex items-center space-x-1'>
+                  <p className='font-bold text-xs '>
+                    {listing.beds > 1 ? `${listing.beds} Beds` : "1 Bed"} 
+                  </p>
+                </div>
+                
+                <div className='flex items-center space-x-1'>
+                  <p className='font-bold text-xs '>
+                    {listing.bath > 1 ? `${listing.bath} Bathrooms`: "1 Bath"}
+                  </p>
+                </div>
+             </div>
+          </div>
+        </NavLink>
+      </li>
     </>
   )
 }
+
 
 export default ListingItem
