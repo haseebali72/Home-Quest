481d82bc15bfcf983c4126397c8f4bc32e68821c
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-07-04 18:32:37 +0500

    random check of auth.currentuser

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { auth } from '../firebase/firebase'
 
 const Offers = () => {
+  console.log(auth.currentUser)
   return (
-    <div>Offers</div>
+    <>
+    <div>
+      asasas
+    </div>
+    </>
   )
 }
 
