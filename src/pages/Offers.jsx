50df795b6328428da274bff95e998b8846418997
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-07-05 14:53:18 +0500

    commented logg checking

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { auth } from '../firebase/firebase'
 
 const Offers = () => {
-  console.log(auth.currentUser)
+  // console.log(auth.currentUser)
   return (
     <>
     <div>
