e8c6e512a6e0cb022508f6dbd2db745e091ea596
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-07-05 14:50:44 +0500

    made Image Slider dynamic Component

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -1,29 +1,27 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay} from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/bundle'
 import 'swiper/css/pagination';
 
 
-export default function ImageSlider ({imageURLs}){
-  const renderBullet = (index, className)=>{
-    return `<span ${className}="w-24 h-24 text-center text-lg text-black opacity-100 bg-slate-300  m-[5px] active:text-white active:bg-blue-500">` + (index + 1) + '</span>';
-  }
+export default function ImageSlider ({imageURLs, spaceBetween,slidePerView, navigation,autoPlay, pagination,scrollbar, imageStyle,}){
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-      spaceBetween={2}
-      slidesPerView={2}
-      navigation={{enabled : true}}
-      pagination={{clickable : true, renderBullet}}
-      scrollbar={{ draggable: true }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay,EffectFade]}
+      spaceBetween={spaceBetween ? spaceBetween : 2}
+      slidesPerView={slidePerView ? slidePerView : 2}
+      navigation={{enabled : navigation ? navigation : false}}
+      pagination={{clickable : pagination ? pagination : false, type : "progressbar"}}
+      autoplay = {autoPlay? {delay : 2000} : false} 
+      scrollbar={{ draggable: scrollbar? scrollbar : false }}
+      // onSlideChange={() => console.log('slide change')}
+      // onSwiper={(swiper) => console.log(swiper)}
     > 
    
-      {imageURLs.map( imageURL=>(
-        <SwiperSlide>
-          <img src={imageURL} className='w-96 h-[35rem] m-auto'/>
+      {imageURLs.map( (imageURL, index)=>(
+        <SwiperSlide  key={index}>
+          <img src={imageURL} style={imageStyle} />
         </SwiperSlide>
       ))}
     </Swiper>
