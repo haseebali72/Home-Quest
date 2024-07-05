5f94c1b880d1b89ac8f3fada88a08b973b0e60e1
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-07-05 14:50:09 +0500

    created dynamic imports by lazy and Suspence

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Offers from './pages/Offers'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import ForgotPassword from './pages/ForgotPassword'
 import Header from './Components/Header'
 import PrivateRoute from './Components/PrivateRoute'
-import CreateListing from './pages/CreateListing'
-import ErrorPage from './pages/ErrorPage'
-import EditListing from './pages/EditListing'
-import Listing from './pages/Listing'
+import {ColorCircleLoaderFull} from './Components/Loader'
+
+const Home = lazy(() => import('./pages/Home'))
+const Profile = lazy(() => import('./pages/Profile'))
+const Offers = lazy(() => import('./pages/Offers'))
+const Signin = lazy(() => import('./pages/Signin'))
+const Signup = lazy(() => import('./pages/Signup'))
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+const CreateListing = lazy(() => import('./pages/CreateListing'))
+const ErrorPage = lazy(() => import('./pages/ErrorPage'))
+const EditListing = lazy(() => import('./pages/EditListing'))
+const Listing = lazy(() => import('./pages/Listing'))
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/profile' element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-        </Route>
-        <Route path='/create-listing' element={<PrivateRoute />}>
-          <Route path='/create-listing' element={<CreateListing />} />
-        </Route>
-        <Route path='/edit-listing' element={<PrivateRoute />}>
-          <Route path='/edit-listing/:listingId' element={<EditListing />} />
-        </Route>
-        <Route path='/sign-in' element={<Signin />} />
-        <Route path='/sign-up' element={<Signup />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/offers' element={<Offers />} />
-        <Route path='/category/:x/:listingId' element={<Listing/>} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-
+      <Suspense fallback={<ColorCircleLoaderFull />}>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path='/create-listing' element={<PrivateRoute />}>
+            <Route path='/create-listing' element={<CreateListing />} />
+          </Route>
+          <Route path='/edit-listing' element={<PrivateRoute />}>
+            <Route path='/edit-listing/:listingId' element={<EditListing />} />
+          </Route>
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:x/:listingId' element={<Listing />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </>
-
   )
 }
 
